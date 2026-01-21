ea9801e235e9969a08f28e46833e1f6a22e98461
syam1310 <psyam0175@gmail.com>
2026-01-21 10:56:12 +0530

    Adding JUnit test

--- src/test/java/com/example/uploading_files/DemoApplicationTests.java
+++ src/test/java/com/example/uploading_files/DemoApplicationTests.java
@@ -1,13 +1,63 @@
 package com.example.uploading_files;
 
+import com.example.uploading_files.exception.StorageFileNotFoundException;
+import com.example.uploading_files.service.StorageService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.stream.Stream;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	MockMvc mockMvc;
 
+	@MockitoBean
+	StorageService storageService;
+
+	@Test
+	void shouldListAllFiles() throws Exception {
+		given(this.storageService.loadAll())
+				.willReturn(Stream.of(
+						Paths.get("first.txt"),
+						Paths.get("second.txt")
+				));
+
+		this.mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(model().attribute("files", Matchers.contains("http://localhost/files/first.txt",
+						"http://localhost/files/second.txt")));
+	}
+	@Test
+	public void shouldSaveUploadedFile() throws Exception {
+		MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt",
+				"text/plain", "Spring Framework".getBytes());
+		this.mockMvc.perform(multipart("/").file(multipartFile))
+				.andExpect(status().isFound())
+				.andExpect(header().string("Location", "/"));
+		then(this.storageService).should().store(multipartFile);
+	}
+	@SuppressWarnings("unchecked")
+	@Test
+	public void should404WhenMissingFile() throws Exception {
+		given(this.storageService.loadAsResource("test.txt"))
+				.willThrow(StorageFileNotFoundException.class);
+
+		this.mockMvc.perform(get("/files/test.txt")).andExpect(status().isNotFound());
+	}
 }
